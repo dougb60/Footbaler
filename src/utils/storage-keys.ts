4221f209ed7ec7f6003386682697e3c156719c87
Douglas Aparecido de Oliveira <douglas.oliveira@db1.com.br>
2022-08-31 19:44:45 -0300

    feat:include storage keys

--- src/utils/storage-keys.ts
+++ src/utils/storage-keys.ts
@@ -1,4 +1,9 @@
 export enum StorageKeys {
+  // Leagues
   LEAGUE_STORAGE = '@Footballer/set_league',
   LEAGUE_LAST_GET = '@Footballer/last_league',
+
+  // Standings
+  STANDINGS_STORAGE = '@Footballer/set_standings',
+  STANDINGS_LAST_GET = '@Footballer/last_standings',
 }
